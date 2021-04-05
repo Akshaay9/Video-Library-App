8f19fdeb17f795527aacff58350ba5b1dc1b3d84
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-04-05 13:56:49 +0530

    bug-fix

--- src/Screens/BodyBuildingScreen/BodyBuildingScreenBanner.js
+++ src/Screens/BodyBuildingScreen/BodyBuildingScreenBanner.js
@@ -3,7 +3,10 @@ import bodyBuildingVideo from "./videoplayback (online-video-cutter.com).mp4";
 function BodyBuildingScreenBanner() {
   return (
     <div>
-      <video className="bodybuilding-banner-video" src={bodyBuildingVideo} autoPlay muted loop></video>
+      <video className="bodybuilding-banner-video" loop="true" autoplay="autoplay" muted="true" allowfullscreen>
+
+      <source type="video/mp4" src={bodyBuildingVideo}></source>
+      </video>
     </div>
   );
 }
