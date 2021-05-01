569de06123b92494e07ea3e34205baff8d386625
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-05-01 10:51:50 +0530

    made video play once loaded else show thumbnail

--- src/Screens/BodyBuildingScreen/BodyBuildingScreenBanner.js
+++ src/Screens/BodyBuildingScreen/BodyBuildingScreenBanner.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import bodyBuildingVideo from "./videoplayback (online-video-cutter.com).mp4";
 function BodyBuildingScreenBanner() {
+  const [videoLoad, setVideoLoad] = useState(false);
+
+  const onVideoLoad = () => {
+  
+    setVideoLoad(true);
+  };
   return (
     <div>
-      <video className="bodybuilding-banner-video" loop="true" autoplay="autoplay" muted="true" allowfullscreen>
+      <div style={{ display: videoLoad ? "none" : "block" }} className="bodynuilding-bg-image"></div>
 
-      <source type="video/mp4" src={bodyBuildingVideo}></source>
+      <video
+        className="bodybuilding-banner-video"
+        loop="true"
+        autoplay="autoplay"
+        muted="true"
+        allowfullscreen
+        onLoadedData={onVideoLoad}
+        style={{ display: videoLoad ? "block" : "none" }}
+      >
+        <source type="video/mp4" src={bodyBuildingVideo}></source>
       </video>
     </div>
   );
