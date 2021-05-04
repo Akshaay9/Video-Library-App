a4619c64e1a7bc1650d2f3ad4ab23c9416c8a7ba
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-05-04 10:18:48 +0530

    mobile responsive toogle nav bar has been added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./Screens/HomeScreen/Index";
 import NavBar from "./Screens/HomeScreen/NavBar";
 import BodyBuildingScreen from "./Screens/BodyBuildingScreen/index";
-import FatLoss from "./Screens/Fatloss/Index"
-import Calisthetic from "./Screens/Calisthetics/Index"
-import Yoga from "./Screens/Yoga/Index"
-import Zoomba from "./Screens/Zoomba/Index"
+import FatLoss from "./Screens/Fatloss/Index";
+import Calisthetic from "./Screens/Calisthetics/Index";
+import Yoga from "./Screens/Yoga/Index";
+import Zoomba from "./Screens/Zoomba/Index";
 import PlayListComponent from "./Components/PlayListComponent/Index";
 import IndividualPlayListComponent from "./Components/IndividualPlaylistComponent/Index";
 import LikedVideos from "./Components/LikedVideoComponent/Index";
@@ -15,28 +15,35 @@ import WatchLaterVideos from "./Components/WatchLaterComponent/Index";
 import IndividualVideosOfPlayList from "./Components/IndividualVideoFromPlayList/Index";
 import IndividualVideo from "./Components/individualVideo/Index";
 function App() {
+  const [navModal, setNavModal] = useState(false);
   return (
+    <div style={navModal?{ height: "99vh", overflow: "hidden" }:{} }>
     <BrowserRouter>
-      <NavBar />
+      <NavBar navModal={navModal} setNavModal={setNavModal} />
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/video/:id" element={<IndividualVideo />} />
         <Route path="/likedvideo" element={<LikedVideos />} />
         <Route path="/WatchLaterVideos" element={<WatchLaterVideos />} />
         <Route path="/playlists" element={<PlayListComponent />} />
-        <Route  path="/playlists/:playListid"  element={<IndividualPlayListComponent />} />
-        <Route path="/playlists/:playListid/:videoid" element={<IndividualVideosOfPlayList />} />
-        
+        <Route
+          path="/playlists/:playListid"
+          element={<IndividualPlayListComponent />}
+        />
+        <Route
+          path="/playlists/:playListid/:videoid"
+          element={<IndividualVideosOfPlayList />}
+        />
+
         {/* videos pages */}
         <Route path="/videos/bodybuilding" element={<BodyBuildingScreen />} />
         <Route path="/videos/fatloss" element={<FatLoss />} />
         <Route path="/videos/calisthetic" element={<Calisthetic />} />
         <Route path="/videos/yoga" element={<Yoga />} />
         <Route path="/videos/zoomba" element={<Zoomba />} />
-
-
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+      </div>
   );
 }
 
--- src/Screens/HomeScreen/NavBar.js
+++ src/Screens/HomeScreen/NavBar.js
@@ -4,7 +4,7 @@ import { useLikedVideoContext } from "../../Context/LikedVideoContext/LikedVideo
 import { UsePlayListContext } from "../../Context/PlaylistContext/PlayListContext";
 import { useWatchLaterContext } from "../../Context/WatchLaterVideoContext/WatchLaterVideoContext";
 
-function NavBar() {
+function NavBar({ navModal, setNavModal }) {
   const {
     state: { likedVideo },
   } = useLikedVideoContext();
@@ -16,64 +16,19 @@ function NavBar() {
     state: { watchLaterVideo },
   } = useWatchLaterContext();
   return (
-    <div className="nav">
-      <div className="nav_left">
-        <div className="nav_logo">
-          <img src="" alt="" />
-        </div>
-        <div className="nav_name">
-          <NavLink to="/">
-            {" "}
+    <>
+      <div className="nav">
+        <div className="nav_left">
+          <div className="nav_logo">
+            <img src="" alt="" />
+          </div>
+
+          <div className="nav_name mobile-hide">
             <NavLink to="/">
-              {" "}
               <h2>Gym.Fit</h2>
             </NavLink>
-          </NavLink>
-        </div>
-      </div>
-      <div className="nav_center">
-        <ul>
-          <NavLink to="/videos/bodybuilding">
-            <li className="hr-underline-middle">Body Building</li>
-          </NavLink>
-          <NavLink to="/videos/fatloss">
-            <li className="hr-underline-middle">Fat Loss</li>
-          </NavLink>
-          <NavLink to="/videos/calisthetic">
-            <li className="hr-underline-middle">Calisthenics</li>
-          </NavLink>
-          <NavLink to="/videos/yoga">
-            <li className="hr-underline-middle">Yoga</li>
-          </NavLink>
-          <NavLink to="/videos/zoomba">
-            <li className="hr-underline-middle">Zumba</li>
-          </NavLink>
-        </ul>
-      </div>
-      <div className="nav_right">
-        <NavLink to="/WatchLaterVideos">
-          <div className="nav_login">
-            <div className="badge badge-skyBlue">
-              <i class="far fa-clock watch-later-icon"></i>
-              {watchLaterVideo.length > 0 && (
-                <span>{watchLaterVideo.length}</span>
-              )}
-            </div>
           </div>
-        </NavLink>
-        <NavLink to="/likedvideo">
-          <div className="nav_cart">
-            <div className="badge badge-skyBlue">
-              <i class="fas fa-heart"></i>
-              {likedVideo.length > 0 && (
-                <span style={{ left: ".9rem" }}>{likedVideo.length}</span>
-              )}
-            </div>
-          </div>
-        </NavLink>
-
-        <NavLink to="/playlists">
-          <div className="badge badge-skyBlue">
+          <div className="badge badge-skyBlue desktop-hide ">
             {playLists.length > 0 && (
               <span style={{ top: "1.1rem", left: "1.5rem" }}>
                 {playLists.length}
@@ -84,9 +39,119 @@ function NavBar() {
               src="https://img.icons8.com/ios/50/000000/video-playlist.png"
             />
           </div>
-        </NavLink>
+          <div
+            className={`hamberger desktop-hide ${navModal ? "rotateHam" : ""}`}
+            onClick={() => setNavModal(!navModal)}
+          >
+            <div className="ham1"></div>
+            <div className="ham2"></div>
+            <div className="ham3"></div>
+          </div>
+        </div>
+        <div className="nav_center">
+          <ul>
+            <NavLink to="/videos/bodybuilding">
+              <li className="hr-underline-middle">Body Building</li>
+            </NavLink>
+            <NavLink to="/videos/fatloss">
+              <li className="hr-underline-middle">Fat Loss</li>
+            </NavLink>
+            <NavLink to="/videos/calisthetic">
+              <li className="hr-underline-middle">Calisthenics</li>
+            </NavLink>
+            <NavLink to="/videos/yoga">
+              <li className="hr-underline-middle">Yoga</li>
+            </NavLink>
+            <NavLink to="/videos/zoomba">
+              <li className="hr-underline-middle">Zumba</li>
+            </NavLink>
+          </ul>
+        </div>
+        <div className="nav_right">
+          <NavLink to="/WatchLaterVideos">
+            <div className="nav_login">
+              <div className="badge badge-skyBlue ">
+                <i class="far fa-clock watch-later-icon"></i>
+                {watchLaterVideo.length > 0 && (
+                  <span>{watchLaterVideo.length}</span>
+                )}
+              </div>
+            </div>
+          </NavLink>
+          <NavLink to="/likedvideo">
+            <div className="nav_cart">
+              <div className="badge badge-skyBlue">
+                <i class="fas fa-heart"></i>
+                {likedVideo.length > 0 && (
+                  <span style={{ left: ".9rem" }}>{likedVideo.length}</span>
+                )}
+              </div>
+            </div>
+          </NavLink>
+
+          <div className="badge badge-skyBlue mobile-hide">
+            <NavLink to="/playlists">
+              {playLists.length > 0 && (
+                <span style={{ top: "1.1rem", left: "1.5rem" }}>
+                  {playLists.length}
+                </span>
+              )}
+              <img
+                style={{ width: "52%", objectFit: "contain", height: "2rem" }}
+                src="https://img.icons8.com/ios/50/000000/video-playlist.png"
+              />
+            </NavLink>
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div
+        className={`mobile_nav_bar ${navModal ? "show_nav" : ""} desktop-hide`}
+      >
+        <ul>
+          <NavLink to="/videos/bodybuilding">
+            <li
+              className="hr-underline-middle"
+              onClick={() => setNavModal(false)}
+            >
+              Body Building
+            </li>
+          </NavLink>
+          <NavLink to="/videos/fatloss">
+            <li
+              className="hr-underline-middle"
+              onClick={() => setNavModal(false)}
+            >
+              Fat Loss
+            </li>
+          </NavLink>
+          <NavLink to="/videos/calisthetic">
+            <li
+              className="hr-underline-middle"
+              onClick={() => setNavModal(false)}
+            >
+              Calisthenics
+            </li>
+          </NavLink>
+          <NavLink to="/videos/yoga">
+            <li
+              className="hr-underline-middle"
+              onClick={() => setNavModal(false)}
+            >
+              Yoga
+            </li>
+          </NavLink>
+          <NavLink to="/videos/zoomba">
+            <li
+              className="hr-underline-middle"
+              onClick={() => setNavModal(false)}
+            >
+              Zumba
+            </li>
+          </NavLink>
+        </ul>
+      </div>
+    </>
   );
 }
 {
