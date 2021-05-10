ff515af2a162d3df0db8ddb58f4b7a784b86faaf
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-05-10 11:10:58 +0530

    added login page and added error handling to ech input items

--- src/Components/UserAccount/Login.js
+++ src/Components/UserAccount/Login.js
@@ -1,47 +1,111 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import { NavLink } from "react-router-dom";
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    var re = /\S+@\S+\.\S+/;
+    if (email.length == 0) {
+      setEmailError("This field is required");
+    } else if (!re.test(email)) {
+      setEmailError("Not and valid email");
+    } else {
+      setEmailError("");
+    }
+  }, [email]);
+
+  useEffect(() => {
+    let special = /[\W]{1,}/;
+    if (password.length == 0) {
+      setPasswordError("this field is required");
+    } else if (password.length < 5) {
+      setPasswordError("passowrd should contain min 6 char");
+    } else if (password.search(/[A-Z]/) < 0) {
+      setPasswordError("password should contain one UpperCase");
+    } else if (password.search(/[a-z]/) < 0) {
+      setPasswordError("password should contain one LowerCase");
+    } else if (password.search(/[0-9]/) < 0) {
+      setPasswordError("password should contain one number");
+    } else if (!special.test(password)) {
+      setPasswordError("password should contain one special char");
+    } else {
+      setPasswordError("");
+    }
+  }, [password]);
+
   return (
     <>
-      <div className="bg-wallper login-bg-wallper ">
-        <div className="login-card">
-          <div className="login-card-left">
-            <img
-              src="https://images.unsplash.com/photo-1580086319619-3ed498161c77?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Z3ltfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=160"
-              alt=""
-            />
-            <div className="login-card-left-heading">
-              <h1>
-                One stop destination to kickstart your <span>fitness</span>{" "}
-                journey
-              </h1>
-            </div>
-            <div className="login-card-left-desc">
-              <ul>
-                <li>Make your body stronger</li>
-                <li>Loose yourself in Ambience</li>
-                <li>Commit to be fit</li>
-                <li>Train your body in fun way</li>
-              </ul>
-            </div>
-          </div>
+      <div className="bg-wallper signup-wallper">
+        <div className="login-card signup-card">
           <div className="login-card-right">
-            <h1>Create your account</h1>
+            <h1>Log In to your account</h1>
             <p>
-              Alredy have an account?{" "}
-              <span style={{ textDecoration: "underline",color:"black" }}>Login in here</span>{" "}
+              Dont have an account?{" "}
+              <span style={{ textDecoration: "underline", color: "black" }}>
+                {" "}
+                <NavLink to="/signup">Sign in here</NavLink>{" "}
+              </span>{" "}
             </p>
             <span className="mini-info-login">or sign up with an email</span>
             <form>
-              <div className="form-top">
-                <input type="text" required placeholder="name" />
-                <input type="email" required placeholder="email" />
+              <div className="form-top login-form-top sign-up-form-top">
+                <input
+                  type="email"
+                  required
+                  placeholder="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  pattern="[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                />
               </div>
+              {/* {errors} */}
+              <div className="error-div-input">
+                {emailError !== "" ? (
+                  <p className="error-handler-input error">
+                    {emailError}
+                    <i className="fas fa-exclamation-circle"></i>
+                  </p>
+                ) : (
+                  <p className="error-handler-input sucess">
+                    Success<i className="fas fa-check-circle"></i>
+                  </p>
+                )}
+              </div>
+              {/* {} */}
               <div className="form-mid">
-                <input type="password" required placeholder="password" />
+                <input
+                  type="password"
+                  required
+                  placeholder="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$"
+                />
+              </div>
+              <div className="error-div-input">
+                {passwordError !== "" ? (
+                  <p className="error-handler-input error">
+                    {passwordError}
+                    <i className="fas fa-exclamation-circle"></i>
+                  </p>
+                ) : (
+                  <p className="error-handler-input sucess">
+                    Success<i className="fas fa-check-circle"></i>
+                  </p>
+                )}
               </div>
               <input type="checkbox" required />
-              <label htmlFor="" style={{fontFamily:"sans-serif",fontSize:"13px"}}>I agree with Terms and conditions</label>
+              <label
+                htmlFor=""
+                style={{ fontFamily: "sans-serif", fontSize: "13px" }}
+              >
+                I agree with Terms and conditions
+              </label>
               <button>Sign up</button>
             </form>
           </div>
@@ -50,4 +114,5 @@ function Login() {
     </>
   );
 }
+
 export default Login;
