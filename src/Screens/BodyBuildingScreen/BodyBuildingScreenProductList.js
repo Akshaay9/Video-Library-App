0eff83466515e257f40b4407efdd1cf21d89eb05
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-05-11 07:15:38 +0530

    Front end bug fix:While calling the product list

--- src/Screens/BodyBuildingScreen/BodyBuildingScreenProductList.js
+++ src/Screens/BodyBuildingScreen/BodyBuildingScreenProductList.js
@@ -2,21 +2,23 @@ import React, { useEffect } from "react";
 import { useVideosContext } from "../../Context/VideoLists/VideoContext";
 import { beginnerBodyBuilding } from "../../Data/BeginnerBodyBuildingData";
 import VideoListCOmponent from "../../UtilityFunctions/VideoListCOmponent";
-import {makeAnAPICall} from "../../APICalls"
+import { makeAnAPICall } from "../../APICalls";
 function BodyBuildingScreenProductList() {
   const {
     state: { bodyBuildingVideo, bodyBuildingLoading },
     videoDIspatch,
   } = useVideosContext();
 
-  useEffect(() => {
-    (async () => {
-      const data = await makeAnAPICall(
-        "GET",
-        
-  )
-})()
-})
+  // useEffect(() => {
+  //   (async () => {
+  //     const data = await makeAnAPICall(
+  //       "GET",
+  //       `https://cryptic-hamlet-94693.herokuapp.com/api/videos/bodybuilding`,
+
+  //     );
+  //     console.log(data.data);
+  //   })();
+  // });
 
   return (
     <>
